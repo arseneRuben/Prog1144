4177579249b56522c82863c2701d455d7b92edd0
emlep06 <martin_lepage03@hotmail.com>
2024-05-29 14:41:10 -0300

    database

--- db.sql
+++ db.sql
@@ -1,22 +1,12 @@
-CREATE DATABASE IF NOT EXISTS notes_app;
-use notes_app;
+CREATE DATABASE IF NOT EXISTS podcast;
+use podcast;
 
-CREATE TABLE IF NOT EXISTS `notes` (
-    `id` integer   auto_increment,
-  `title` varchar(255) NOT NULL,
-  `contents` text NOT NULL,
-  `created` timestamp NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS podcasts (
+  `id` INT auto_increment PRIMARY KEY,
+  `title` varchar(255) not null,
+  `description` text,
+  `contents` text,
+  `created_at` timestamp default current_timestamp
 );
-CREATE TABLE IF NOT EXISTS `codeurs` (
-    `id` integer   auto_increment,
-  `firstname` varchar(255) NOT NULL,
-  `lastname` text NOT NULL,
-  `experience` ENUM ('JUNIOR', 'INTERMEDIAIRE', 'SEINIOR'),
-  PRIMARY KEY (`id`)
-);
-ALTER TABLE notes ADD column idCodeur int DEFAULT(1);
-ALTER TABLE notes ADD FOREIGN KEY fk_notes_codeurs(idCodeur) REFERENCES codeurs(id);
 
 
-select * from notes where idCodeur=1;
