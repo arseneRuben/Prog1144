a1088b7d3819eee571023155d8d9396120717711
denisemond7 <denisemond4612@gmail.com>
2024-05-31 11:32:52 -0300

    Added all necessary folders

--- client/src/pages/PodCastPage.js
+++ client/src/pages/PodCastPage.js
@@ -1,13 +1,54 @@
-import React from 'react'
-import "bootstrap/dist/css/bootstrap.min.css"
-import { Button } from 'bootstrap'
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col, Table } from 'react-bootstrap';
 
 const PodCastPage = () => {
+  const [podcasts, setPodcasts] = useState([]);
+
+  useEffect(() => {
+    fetchPodcasts();
+  }, []);
+
+  const fetchPodcasts = () => {
+    fetch('http://localhost:5000/podcasts')
+      .then(response => response.json())
+      .then(data => setPodcasts(data))
+      .catch(error => console.error('Error fetching podcasts:', error));
+  };
+
   return (
-    <h2>PodCastPage</h2>
-    
-  )
+    <Container>
+      <Row className="justify-content-md-center">
+        <Col md="8">
+          <h2>Podcast Page</h2>
+          <Table striped bordered hover className="mt-4">
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Description</th>
+                <th>Filename</th>
+                <th>Language</th>
+                <th>Id Program</th>
+                <th>Id Presentation</th>
+              </tr>
+            </thead>
+            <tbody>
+              {podcasts.map((podcast, index) => (
+                <tr key={index}>
+                  <td>{podcast.title}</td>
+                  <td>{podcast.descriptions}</td>
+                  <td>{podcast.filename}</td>
+                  <td>{podcast.langue}</td>
+                  <td>{podcast.id_program}</td>
+                  <td>{podcast.id_presentation}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
-}
-
-export default PodCastPage
+export default PodCastPage;
