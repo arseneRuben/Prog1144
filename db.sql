ea44277114b292d7115045fd99f81cb8667026e4
kamalcall <kamalcallme@live.ca>
2024-05-31 13:08:59 -0400

    modified:   db.sql

--- db.sql
+++ db.sql
@@ -1,22 +1,12 @@
-CREATE DATABASE IF NOT EXISTS notes_app;
 use notes_app;
 
-CREATE TABLE IF NOT EXISTS `notes` (
-    `id` integer   auto_increment,
+CREATE TABLE IF NOT EXISTS `program` (
+  `id` integer   auto_increment,
   `title` varchar(255) NOT NULL,
-  `contents` text NOT NULL,
-  `created` timestamp NOT NULL DEFAULT current_timestamp(),
+  `description` text NOT NULL,
   PRIMARY KEY (`id`)
 );
-CREATE TABLE IF NOT EXISTS `codeurs` (
-    `id` integer   auto_increment,
-  `firstname` varchar(255) NOT NULL,
-  `lastname` text NOT NULL,
-  `experience` ENUM ('JUNIOR', 'INTERMEDIAIRE', 'SEINIOR'),
-  PRIMARY KEY (`id`)
-);
-ALTER TABLE notes ADD column idCodeur int DEFAULT(1);
-ALTER TABLE notes ADD FOREIGN KEY fk_notes_codeurs(idCodeur) REFERENCES codeurs(id) ON DELETE CASCADE;
 
 
-select * from notes ;
+
+select * from program ;
