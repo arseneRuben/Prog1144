1c432bd57a360cd340999bd7d209d7c76f5f2616
emlep06 <martin_lepage03@hotmail.com>
2024-05-31 12:29:35 -0300

    daomysql

--- src/connectors/daoMySql.js
+++ src/connectors/daoMySql.js
@@ -1,36 +1,50 @@
+'use strict';
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+dotenv.config();
 
-'use strict'
-import mysql from 'mysql2'
-import dotenv from 'dotenv'
-dotenv.config({ })
+console.log('Environment Variables:', {
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+});
 
-let connection = {}
+let connection = {};
 
-export function connect () {
+export function connect() {
     connection = mysql.createConnection({
-        host:  process.env.MYSQL_HOST ,
-        user: process.env.MYSQL_USER ,
-        password: process.env.MYSQL_PASSWORD ,
+        host: process.env.MYSQL_HOST,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
         database: process.env.MYSQL_DATABASE,
-    })
+    });
+
     connection.connect((err) => {
         if (err) {
-            console.log('Error connecting to Db')
-            throw err
+            console.error('Error connecting to Db:', err);
+            throw err;
         }
-        console.log('Connection ESTABLISHED')
-    })
-}
-export function query (query, values, resultCallback) {
-    connection.query(query, values, (error, result) => {
-        if (error) {
-            throw error
-        }
-        resultCallback(result)
-    })
+        console.log('Connection ESTABLISHED');
+    });
 }
 
-export function disconnect () {
-    console.log('Connection ENDED')
-    connection.end()
+export function query(query, values, resultCallback) {
+    connection.query(query, values, (error, result) => {
+        if (error) {
+            console.error('Error executing query:', error);
+            throw error;
+        }
+        resultCallback(result);
+    });
+}
+
+export function disconnect() {
+    console.log('Connection ENDED');
+    connection.end((err) => {
+        if (err) {
+            console.error('Error ending connection:', err);
+            throw err;
+        }
+    });
 }
