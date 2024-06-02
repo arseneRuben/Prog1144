17d50d7be88821fd7755d20ca29555c593c2d2b8
arseneruben <fopoar@gmail.com>
2024-06-01 21:47:11 -0400

    mise a jour de la db

--- db.sql
+++ db.sql
@@ -32,13 +32,41 @@ CREATE TABLE IF NOT EXISTS podcasts (
     
 );
 
+
 insert into podcasts(title, descriptions, filename, langue, id_program, id_presentation) values ("Lionel Messi", "Premiere arriver de Lionel Messi a Montreal", "messi.png", "fr", 1, null);
+insert into podcasts(title, descriptions, filename, langue, id_program, id_presentation) values ("Second Podcast", "This is a second Podcast", "podcast2.mp3", "fr", 2, 101);
+insert into podcasts(title, descriptions, filename, langue, id_program, id_presentation) values ("Third Podcast", "This is a third Podcast", "podcast3.mp3", "fr", 3, 102);
 
 select * from podcasts; 
+
 
 ALTER TABLE notes ADD column idCodeur int DEFAULT(1);
 ALTER TABLE notes ADD FOREIGN KEY fk_notes_codeurs(idCodeur) REFERENCES codeurs(id) ON DELETE CASCADE;
 
 
 select * from notes ;
+
+CREATE TABLE `event` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `description` text,
+  `date` timestamp NOT NULL,
+  `location` varchar(255) DEFAULT NULL,
+  `createdBy` int NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  KEY `fk_animator` (`createdBy`),
+  CONSTRAINT `fk_animator` FOREIGN KEY (`createdBy`) REFERENCES `animator` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `animator` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `login` varchar(255) NOT NULL,
+  `pwd` varchar(255) NOT NULL,
+  `full_name` varchar(255) DEFAULT NULL,
+  `google_id` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 
